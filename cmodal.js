6aaac0858d80f4b21d861833493e72d88db230ae
Leonardo Fogaça <leonardogfogaca07@gmail.com>
2025-02-19 17:54:34 -0300

    a

--- cmodal.js
+++ cmodal.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const modal = document.getElementById("contact-cmodal");
     const closeButton = document.querySelector(".cclose");
+    const iframe = document.getElementById("contact-frame");
 
     if (closeButton) {
         closeButton.addEventListener("click", function () {
@@ -11,9 +12,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function openModal(id) {
     const modal = document.getElementById(id);
+    const iframe = document.getElementById("contact-frame");
+    
     if (modal) {
         modal.style.display = "block";
-        document.getElementById("contact-frame").src = "other-pages/nosContate.html"; // Garante que o iframe carregue
+        if (iframe) {
+            iframe.src = iframe.src; // Recarrega o iframe
+        }
     }
 }
 
